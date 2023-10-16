90686f603c3f0cb656bd873d83924d1c45c217e2
John-Pratt <johnw_pratt@outlook.com>
2023-10-16 15:32:32 -0500

    Updated About Page

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -24,8 +24,8 @@ const AboutSectionOne = () => {
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="Crafted for Startup, SaaS and Business Sites."
-                paragraph="The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables."
+                title="Welcome to NDev"
+                paragraph="We can make shit happen fast and in a hurry.  If you want to join the band just shoot an email."
                 mb="44px"
               />
 
